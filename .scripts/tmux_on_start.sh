941792319c41832f61854aaeb4c3741746465413
Joe R <jvr2424@gmail.com>
2024-12-06 15:26:21 -0500

    updated tmux conf

--- .scripts/tmux_on_start.sh
+++ .scripts/tmux_on_start.sh
@@ -2,24 +2,31 @@
 
 local session_name="default"
 
-# Create a new tmux session in the background
-tmux new-session -d -s "$session_name"
+if tmux has-session -t $session_name 2>/dev/null; then
+  if [ -z "$TMUX" ]; then
+    echo "Session $session_name already exists, attching to it"
+    tmux attach-session -t "$session_name"
+  fi
 
-# First window: Home directory
-tmux send-keys -t "$session_name:1" "cd ~ && clear" C-m
+else
 
-# Second window: First specified directory
-tmux new-window -t "$session_name:2"
-tmux send-keys -t "$session_name:2" "cd ~/Documents/ios_app/workout-tracker && clear" C-m
+  # Create a new tmux session in the background
+  tmux new-session -d -s "$session_name"
 
-# Third window: Second specified directory
-tmux new-window -t "$session_name:3"
-tmux send-keys -t "$session_name:3" "cd ~/Documents/Projects/macro_tracker && clear" C-m
+  # First window: Home directory
+  tmux send-keys -t "$session_name:1" "cd ~ && clear" C-m
 
-# Select the first window
-tmux select-window -t "$session_name:2"
+  tmux new-window -t "$session_name:2"
+  tmux send-keys -t "$session_name:2" "cd ~/Documents/ios_app/workout-tracker && clear" C-m
 
-# Attach to the session if not already in a tmux session
-if [ -z "$TMUX" ]; then
-  tmux attach-session -t "$session_name"
+  tmux new-window -t "$session_name:3"
+  tmux send-keys -t "$session_name:3" "cd ~/Documents/Projects/macro_tracker && clear" C-m
+
+  # Select the main window
+  tmux select-window -t "$session_name:2"
+
+  if [ -z "$TMUX" ]; then
+    tmux attach-session -t "$session_name"
+  fi
+
 fi
