a9dd8991b4a7344f353f4bb2739040ef10f63f73
Joe R <jvr2424@gmail.com>
2024-12-09 15:28:38 -0500

    added formatting keymap

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -68,3 +68,6 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("v", "<", "<gv")
 vim.keymap.set("v", ">", ">gv")
+
+--format file
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
