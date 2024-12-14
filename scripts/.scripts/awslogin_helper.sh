49609cec4dc0e3378a20e0d621bd6094818afde2
Joe R <jvr2424@gmail.com>
2024-12-14 13:35:01 -0500

    removed unneeded scripts

--- scripts/.scripts/awslogin_helper.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source "$HOME/.dotfiles/.env"
-
-role=$( printf "$POSSIBLE_AWS_ROLES" | fzf --height=10% --border --border-label "$CURRENT_AWS_ACCOUNT_NAME - $CURRENT_AWS_ACCOUNT_ID")
-
-awslogin file -a $CURRENT_AWS_ACCOUNT_ID -r $role -f
-
