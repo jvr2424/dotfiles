484656edd20a8760f00774f6c64a44842429af8f
Joe R <jvr2424@gmail.com>
2025-12-13 17:17:13 -0500

    change to gitopr with recent github url change

--- scripts/.scripts/git_open_remote.sh
+++ scripts/.scripts/git_open_remote.sh
@@ -33,7 +33,8 @@ function get_git_pr_search_url() {
     local git_remote=$(get_git_remote_remove_suffix)
     local git_branch_search=$(get_git_branch | sed 's/\//%2f/g')
 
-    echo "$git_remote/pulls?q=is%3Apr+is%3Aopen+$git_branch_search"
+    # echo "$git_remote/pulls?q=is%3Apr+is%3Aopen+$git_branch_search"
+    echo "$git_remote/pulls?q=is%3Apr+is%3Aopen+head%3A$git_branch_search"
 }
 
 function get_git_branch_url() {
