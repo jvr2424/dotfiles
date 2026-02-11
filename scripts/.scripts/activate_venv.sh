7a6f8fee6a78ba16e686ca9a9b4d9df0e94e5b3e
Joe R <jvr2424@gmail.com>
2026-02-10 20:34:56 -0500

    ensure acvenv looks at the root of the git repo

--- scripts/.scripts/activate_venv.sh
+++ scripts/.scripts/activate_venv.sh
@@ -1,4 +1,5 @@
-ACTIVATE_SCRIPT=$(fd --no-ignore --hidden -a --glob activate)
+# find the activate scripts by searching for activate from the root of the git dir
+ACTIVATE_SCRIPT=$(fd --no-ignore --hidden -a --glob activate $(git rev-parse --show-toplevel))
 SCRIPT_COUNT=$(echo "${ACTIVATE_SCRIPT}" | wc -l | xargs)
 if [ $SCRIPT_COUNT != "1" ]; then
   ACTIVATE_SCRIPT=$(echo "${ACTIVATE_SCRIPT}" | fzf)
@@ -6,3 +7,5 @@ fi
 
 
 source "${ACTIVATE_SCRIPT}"
+
+
